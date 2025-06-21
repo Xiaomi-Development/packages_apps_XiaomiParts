6a55ad2eb83ba848a0cd94778a84470a44776083
TechPanelGM <giomook2003@gmail.com>
2025-06-21 18:13:10 +0000

    XiaomiParts: Add Xiaomi Dirac

--- src/org/lineageos/settings/BootCompletedReceiver.java
+++ src/org/lineageos/settings/BootCompletedReceiver.java
@@ -23,13 +23,21 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.util.Log;
 
+import org.lineageos.settings.dirac.DiracUtils;
+
 public class BootCompletedReceiver extends BroadcastReceiver {
 
     private static final boolean DEBUG = false;
+    private static final String TAG = "XiaomiParts";
 
     @Override
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG)
             Log.d(TAG, "Received boot completed intent");
+        try {
+            DiracUtils.getInstance(context);
+        } catch (Exception e) {
+            Log.d(TAG, "Dirac is not present in system");
+        }
     }
 }
