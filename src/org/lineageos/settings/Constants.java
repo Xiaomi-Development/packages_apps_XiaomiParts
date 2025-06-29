94279023dc9820ae3a0e9d2cf2d7ffff45164757
TechPanelGM <giomook2003@gmail.com>
2025-06-29 13:15:30 +0000

    XiaomiParts: Add Clear Speaker Co-authored-by: Adithya R <radithya2002@gmail.com> Co-authored-by: TheScarastic <warabhishek@gmail.com>
    Co-authored-by: Joey Huab <joey@evolution-x.org>

---
+++ src/org/lineageos/settings/Constants.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2023-2024 The Evolution X Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.settings;
+
+import android.content.Context;
+
+import org.lineageos.settings.R;
+
+public class Constants {
+
+    // Clear Speaker
+    public static final String KEY_CLEAR_SPEAKER = "clear_speaker_pref";
+
+}
